8705c69fa823bf9bebdf58ac485f2ff9b12908ad
Harper Shapiro <harper.shapiro@gmail.com>
2019-12-04 23:47:27 -0800

    added some outline in main, added graph and priority queue

--- MovieLensStarter/src/analyzer/MovieLensAnalyzer.java
+++ MovieLensStarter/src/analyzer/MovieLensAnalyzer.java
@@ -12,7 +12,26 @@ public class MovieLensAnalyzer {
 		if(args.length != 2){
 			System.err.println("Usage: java MovieLensAnalyzer [ratings_file] [movie_title_file]");
 			System.exit(-1);
-		}		
-		// FILL IN THE REST OF YOUR PROGRAM
+		}
+
+
+		//todo:
+		//create graph structure adjList or adjMatrix
+		//get user input for graph options
+		//parse data files, filling graph
+		//get user input for analysis
+		//possible analyses
+//		The number of nodes
+//		The number of edges
+//		The density of the graph defined as D = E / (V*(V-1)) for a directed graph
+//		The maximum degree (i.e. the largest number of outgoing edges of any node)
+//		The diameter of the graph (i.e. the longest shortest path)
+//		The average length of the shortest paths in the graph
+
+		//for Dijkstra's (option 3)
+			//get user input for start and end nodes
+			//run Dijkstra's
+			//print out entire shortest path
+
 	}
 }
--- MovieLensStarter/src/graph/GraphAlgorithms.java
+++ MovieLensStarter/src/graph/GraphAlgorithms.java
@@ -3,4 +3,12 @@ package graph;
 
 public class GraphAlgorithms {
 	// FILL IN
+
+    public static int[][] floydWarshall(Graph<Integer> graph){
+        return null;
+    }
+
+    public static int[] dijkstrasAlgorithm(Graph<Integer> graph, int source){
+        return null;
+    }
 }
