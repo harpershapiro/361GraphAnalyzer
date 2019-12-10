ace1671bf77ad6b45aab6a13b4558308a851af5f
Bret Abel <37429769+bretabel@users.noreply.github.com>
2019-12-09 18:26:58 -0800

    Function Flloyd-Warshall alg

--- MovieLensStarter/src/graph/FlloydWarshall.java
+++ MovieLensStarter/src/graph/FlloydWarshall.java
@@ -27,9 +27,11 @@ public class FlloydWarshall {
 		int dest = 0;
 		try {
 			for (Integer v : graph.getVertices()) {
+				dest = 0;
 				for (Integer u : graph.getVertices()) {
 					if (graph.edgeExists(v,u)) {
 						dist[src][dest] = 1;
+						dist[dest][src] = 1;
 					}
 					dest++;
 				}
@@ -42,8 +44,6 @@ public class FlloydWarshall {
 		// Generate shortest paths by comparing intermediate vertices
 		for (int i = 0; i < V; i++) {
 			for (int j = 0; j < V; j++) {
-				//optimization: ignore nodes that aren't connected by an edge
-				if (dist[i][j] == INF) continue;
 				for (int k = 0; k < V; k++) {
 					if (dist[i][j] + dist[j][k] < dist[i][k]) {
 						dist[i][k] = dist[i][j] + dist[j][k];
@@ -56,17 +56,36 @@ public class FlloydWarshall {
 	
 	public static void main(String[] args) {
 		 Graph<Integer> dtest = new Graph<Integer>();
+		 	System.out.println("Adding vertices...");
 	        dtest.addVertex(5);
 	        dtest.addVertex(16);
 	        dtest.addVertex(12);
 	        dtest.addVertex(8);
+	        System.out.println(dtest.getVertices());
 
+	        System.out.println("Adding edges...");
 	        dtest.addEdge(5,16);
 	        dtest.addEdge(16,12);
 	        dtest.addEdge(12,8);
 	        dtest.addEdge(5,8);
-	        
-	        int[][] dists = floydWarshall(dtest);
-	        System.out.println(Arrays.toString(dists));
+	        System.out.println("Outgoing Edges (direction is ignored by algorithm):");
+		try {
+			System.out.println("16:" + dtest.getNeighbors(16));
+			System.out.println("5:" + dtest.getNeighbors(5));
+			System.out.println("8:" + dtest.getNeighbors(8));
+			System.out.println("12:" + dtest.getNeighbors(12));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		int[][] dists = floydWarshall(dtest);
+	        System.out.println("Flloyd-Warshall shortest paths:");
+	        for (Integer v : dtest.getVertices()) {
+	        	System.out.print("| " + v + " ");
+			} System.out.print("|\n");
+	        for (int[] row : dists) {
+				System.out.println(Arrays.toString(row));
+			}
+
 	}
 }
--- MovieLensStarter/src/graph/GraphAlgorithms.java
+++ MovieLensStarter/src/graph/GraphAlgorithms.java
@@ -12,51 +12,50 @@ public class GraphAlgorithms {
 	 * @param graph The graph to find paths for
 	 * @return 2D array holding shortest paths
 	 */
-	public static int[][] floydWarshall(Graph<Integer> graph){
-		int V = graph.numVertices();
-		int INF = 99999; 
-		// did this as opposed to Integer.MAX_VALUE to avoid
-		// spilling over to 0 when comparing distance
-		
-		//make a 2D array to hold the distances and initialize to infinite
-		int[][] dist = new int[V][V];
-		for (int[] row : dist) {
-			Arrays.fill(row, INF);
-		}
-		
-		// Since we have no weights, reinitialize distances
-		// using existence of edges in graph
-		int src = 0;
-		int dest = 0;
-		try {
-			for (Integer v : graph.getVertices()) {
-				for (Integer u : graph.getVertices()) {
-					if (graph.edgeExists(v,u)) {
-						dist[src][dest] = 1;
-					}
-					dest++;
-				}
-				src++;	
-			}
-		} catch (Exception e){
-			System.out.println("Invalid vertex encountered in Flloyd-Warshall.");
-			return null;
-		}
-		
-		// Generate shortest paths by comparing intermediate vertices
-		for (int i = 0; i < V; i++) {
-			for (int j = 0; j < V; j++) {
-				//optimization: ignore nodes that aren't connected by an edge
-				if (dist[i][j] == INF) continue;
-				for (int k = 0; k < V; k++) {
-					if (dist[i][j] + dist[j][k] < dist[i][k]) {
-						dist[i][k] = dist[i][j] + dist[j][k];
-					}
-				}
-			}
-		}
-      return dist;
-  }
+    public static int[][] floydWarshall(Graph<Integer> graph){
+        int V = graph.numVertices();
+        int INF = 99999;
+        // did this as opposed to Integer.MAX_VALUE to avoid
+        // spilling over to 0 when comparing distance
+
+        //make a 2D array to hold the distances and initialize to infinite
+        int[][] dist = new int[V][V];
+        for (int[] row : dist) {
+            Arrays.fill(row, INF);
+        }
+
+        // Since we have no weights, reinitialize distances
+        // using existence of edges in graph
+        int src = 0;
+        int dest = 0;
+        try {
+            for (Integer v : graph.getVertices()) {
+                dest = 0;
+                for (Integer u : graph.getVertices()) {
+                    if (graph.edgeExists(v,u)) {
+                        dist[src][dest] = 1;
+                        dist[dest][src] = 1; // ignores direction in graph, every edge reciprocated
+                    }
+                    dest++;
+                }
+                src++;
+            }
+        } catch (Exception e){
+            System.out.println("Invalid vertex encountered in Flloyd-Warshall.");
+            return null;
+        }
+        // Generate shortest paths by comparing intermediate vertices
+        for (int i = 0; i < V; i++) {
+            for (int j = 0; j < V; j++) {
+                for (int k = 0; k < V; k++) {
+                    if (dist[i][j] + dist[j][k] < dist[i][k]) {
+                        dist[i][k] = dist[i][j] + dist[j][k];
+                    }
+                }
+            }
+        }
+        return dist;
+    }
 
     /**
      * Returns the shortest paths from source to other vertices in graph.
@@ -128,9 +127,14 @@ public class GraphAlgorithms {
         for(Integer v : prev.keySet()){
             System.out.println("vertex: " + v + " previous: " + prev.get(v));
         }
-        
-        int[][] dists = floydWarshall(dtest);
-        System.out.println(Arrays.toString(dists));
 
+        int[][] dists = floydWarshall(dtest);
+        System.out.println("Flloyd-Warshall shortest paths:");
+        for (Integer v : dtest.getVertices()) {
+            System.out.print("| " + v + " ");
+        } System.out.print("|\n");
+        for (int[] row : dists) {
+            System.out.println(Arrays.toString(row));
+        }
     }
 }
